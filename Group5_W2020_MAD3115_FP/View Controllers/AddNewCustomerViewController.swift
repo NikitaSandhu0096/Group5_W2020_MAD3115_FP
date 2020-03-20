165bdf1757b9811c59a42eeed2731e06783593a0
Nikita Sandhu <sandhunikita0096@gmail.com>
2020-03-19 23:13:21 -0400

    new customer

---
+++ Group5_W2020_MAD3115_FP/View Controllers/AddNewCustomerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddNew CustomerViewController.swift
+//  Group5_W2020_MAD3115_FP
+//
+//  Created by Nikita Sandhu on 2020-03-19.
+//  Copyright © 2020 Nikita Sandhu. All rights reserved.
+//
+
+import UIKit
+
+class AddNewCustomerViewController: UIViewController {
+
+    @IBOutlet weak var lblID: UITextField!
+    @IBOutlet weak var lblFirstName: UITextField!
+    @IBOutlet weak var lblLastName: UITextField!
+    @IBOutlet weak var lblGender: UITextField!
+    @IBOutlet weak var textField_Date: UITextField!
+    @IBOutlet weak var lblMobile: UITextField!
+    @IBOutlet weak var lblEmail: UITextField!
+    @IBOutlet weak var lblUserName: UITextField!
+    @IBOutlet weak var lblPassword: UITextField!
+    @IBOutlet weak var lblAddress: UITextField!
+    @IBOutlet weak var lblCity: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ Group5_W2020_MAD3115_FP/View Controllers/AddNewOwnerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddNewOwnerViewController.swift
+//  Group5_W2020_MAD3115_FP
+//
+//  Created by Nikita Sandhu on 2020-03-19.
+//  Copyright © 2020 Nikita Sandhu. All rights reserved.
+//
+
+import UIKit
+
+class AddNewOwnerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
