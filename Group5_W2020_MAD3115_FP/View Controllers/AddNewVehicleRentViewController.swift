2b56cf7dc52b2c00929f3ff157f05a10308ca232
Nikita Sandhu <sandhunikita0096@gmail.com>
2020-03-23 16:52:49 -0400

    three textfields vehicle rent

--- Group5_W2020_MAD3115_FP/View Controllers/AddNewVehicleRentViewController.swift
+++ Group5_W2020_MAD3115_FP/View Controllers/AddNewVehicleRentViewController.swift
@@ -22,6 +22,9 @@ class AddNewVehicleRentViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var txtNoofKM: UITextField!
     @IBOutlet weak var txtVehicle: UITextField!
     
+    var datePicker1 : UIDatePicker!
+    var datePicker2 : UIDatePicker!
+
     var tempVehicle : Vehicle?
     
     override func viewDidLoad() {
@@ -80,8 +83,91 @@ class AddNewVehicleRentViewController: UIViewController, UITextFieldDelegate {
                     
     func textFieldDidBeginEditing(_ textField: UITextField){
         self.pickDriver(self.txtVehicle)
+        self.pickUpDate1(self.txtRSDate)
+        self.pickUpDate2(self.txtREDate)
     }
-                    
+    
+//    func textFieldDidBeginEditing(_ textField1: UITextField , _ textField2: UITextField){
+//        self.pickUpDate1(self.txtRSDate)
+//        self.pickUpDate2(self.txtREDate)
+//    }
+    
+    func pickUpDate1(_ textField : UITextField){
+        self.datePicker1 = UIDatePicker(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 216))
+        self.datePicker1.backgroundColor = UIColor.white
+        self.datePicker1.datePickerMode = UIDatePicker.Mode.date
+        textField.inputView = self.datePicker1
+          
+        //ToolBar
+        let toolBar = UIToolbar()
+        toolBar.barStyle = .default
+        toolBar.isTranslucent = true
+        toolBar.tintColor = .blue//UIColor(red: 92/255, green: 216/255, blue 255/255, alpha : 1)
+        toolBar.sizeToFit()
+          
+        //Adding Button ToolBar
+          
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(AddNewVehicleRentViewController.doneSClick))
+        
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(AddNewVehicleRentViewController.cancelSClick))
+          
+        toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        textField.inputAccessoryView = toolBar
+    }
+    
+    @objc func doneSClick(){
+        let dateFormatter1 = DateFormatter()
+        dateFormatter1.dateStyle = .medium
+        dateFormatter1.timeStyle = .none
+        txtRSDate.text = dateFormatter1.string(from: datePicker1.date)
+        txtRSDate.resignFirstResponder()
+    }
+     
+    @objc func cancelSClick(){
+        txtRSDate.resignFirstResponder()
+    }
+    
+    func pickUpDate2(_ textField : UITextField){
+        self.datePicker2 = UIDatePicker(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 216))
+        self.datePicker2.backgroundColor = UIColor.white
+        self.datePicker2.datePickerMode = UIDatePicker.Mode.date
+        textField.inputView = self.datePicker2
+          
+        //ToolBar
+        let toolBar = UIToolbar()
+        toolBar.barStyle = .default
+        toolBar.isTranslucent = true
+        toolBar.tintColor = .blue//UIColor(red: 92/255, green: 216/255, blue 255/255, alpha : 1)
+        toolBar.sizeToFit()
+          
+        //Adding Button ToolBar
+          
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(AddNewVehicleRentViewController.doneEClick))
+        
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(AddNewVehicleRentViewController.cancelEClick))
+          
+        toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        textField.inputAccessoryView = toolBar
+    }
+    
+    @objc func doneEClick(){
+        let dateFormatter1 = DateFormatter()
+        dateFormatter1.dateStyle = .medium
+        dateFormatter1.timeStyle = .none
+        txtREDate.text = dateFormatter1.string(from: datePicker1.date)
+        txtREDate.resignFirstResponder()
+    }
+     
+    @objc func cancelEClick(){
+        txtRSDate.resignFirstResponder()
+    }
+    
     func pickDriver(_ textField : UITextField){
         self.vehiclePicker = UIPickerView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 216))
         self.vehiclePicker.delegate = self
@@ -98,24 +184,24 @@ class AddNewVehicleRentViewController: UIViewController, UITextFieldDelegate {
                       
         //Adding Button ToolBar
                       
-        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(AddNewCarViewController.doneClick))
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(AddNewVehicleRentViewController.doneDClick))
                 
         let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
                       
-        let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(AddNewCarViewController.cancelClick))
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(AddNewVehicleRentViewController.cancelDClick))
                       
         toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
         toolBar.isUserInteractionEnabled = true
         textField.inputAccessoryView = toolBar
     }
                      
-    @objc func doneClick(){
+    @objc func doneDClick(){
         tempVehicle = vehicleNames[self.vehiclePicker.selectedRow(inComponent: 0)]
         txtVehicle.text = "\(vehicleNames[self.vehiclePicker.selectedRow(inComponent: 0)].type)  :  \(vehicleNames[self.vehiclePicker.selectedRow(inComponent: 0)].description)"
         txtVehicle.resignFirstResponder()
     }
                      
-    @objc func cancelClick(){
+    @objc func cancelDClick(){
         txtVehicle.resignFirstResponder()
     }
 }
